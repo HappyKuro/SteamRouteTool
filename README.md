8e683fa01294cd16ec935dd83c65fe116f2a30c2
MasterCatPL <107404623+MasterCatPL@users.noreply.github.com>
2024-05-02 21:04:03 +0200

    Version updated

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # SteamRouteTool
-A tool for blocking routes in Steam games that use network_config.json
+A tool for blocking routes in Steam games that use GetSDRConfig
 
 Note: This will automatically clear any TF2RoutingTool firewall rules upon launch (to save the need to clear those rules in that application).
 
 ## Supports:
-Blocking specific routes for games that use Valve's network_config.json (Team Fortress 2 ONLY!).
+Blocking specific routes for games that use Valve's GetSDRConfig (Team Fortress 2 ONLY!).
 
 ## How to use
 * Click the checkbox of the route(s) you'd like to block.
--- SteamRouteTool/Properties/AssemblyInfo.cs
+++ SteamRouteTool/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("A tool for blocking routes in Steam games that use network_config.json")]
+[assembly: AssemblyTitle("A tool for blocking routes in Steam games that use GetSDRConfig")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Froody")]
 [assembly: AssemblyProduct("Steam Route Tool")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
